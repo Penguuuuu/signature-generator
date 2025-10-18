c45dbbff0ab3a7bcbc39aef7348dcb66889f7ce3
Penguuuuu <PenguGithub@gmail.com>
2025-10-19 03:18:40 +1100

    rerwrite canvas.js

--- canvas.js
+++ canvas.js
@@ -2,68 +2,100 @@ export async function createCanvas() {
     const container = document.getElementById('preview');
     container.innerHTML = '';
 
-    const canvas = document.createElement("canvas");
+    const canvas = createBase(350, 20);
+    const context = canvas.getContext('2d');
+
+    drawBackground(context, canvas);
+    drawStripes(context, canvas);
+    await drawImage(context, canvas);
+    drawText(context, canvas);
+    drawShine(context, canvas);
+    drawBorder(context, canvas);
+
+    const canvasLarge = createLargeCanvas(canvas);
+    container.append(canvas, canvasLarge);
+}
+
+function createBase(width, height) {
+    const canvas = document.createElement('canvas');
     canvas.id = 'canvas';
-    canvas.width = 350;
-    canvas.height = 20;
+    canvas.width = width;
+    canvas.height = height;
 
-    const context = canvas.getContext("2d");
+    return canvas;
+}
 
-    const typeBackground = document.getElementById('typeBackground').firstChild.textContent.toLowerCase();
-    if (typeBackground === 'solid') context.fillStyle = '#fff';
+function drawBackground(context, canvas) {
+    const option = document.getElementById('typeBackground').firstChild.textContent.toLowerCase();
+
+    if (option === 'solid') context.fillStyle = '#fff';
     else {
-        const bgGradient = context.createLinearGradient(0, 1, 0, canvas.height - 1);
-        bgGradient.addColorStop(0, 'black');
-        bgGradient.addColorStop(1, 'purple');
-        context.fillStyle = bgGradient;
+        const gradient = context.createLinearGradient(0, 1, 0, canvas.height - 1);
+        gradient.addColorStop(0, 'black');
+        gradient.addColorStop(1, 'purple');
+        context.fillStyle = gradient;
     }
     context.fillRect(0, 0, canvas.width, canvas.height);
+}
 
-    const checkboxStripes = document.getElementById('checkboxStripes');
-    if (checkboxStripes.checked) {
-        const stripeSpacing = 5;
-        const stripeColor = 'rgba(255,255,255,0.2)';
-        const stripeWidth = 1;
+function drawStripes(context, canvas) {
+    const checkbox = document.getElementById('checkboxStripes');
+    if (!checkbox.checked) return;
 
-        context.strokeStyle = stripeColor;
-        context.lineWidth = stripeWidth;
+    const stripeSpacing = 5;
+    const stripeColor = 'rgba(255,255,255,0.2)';
+    const stripeWidth = 1;
 
-        for (let x = 0; x <= canvas.width + canvas.height; x += stripeSpacing) {
-            context.beginPath();
-            context.moveTo(x, 0);
-            context.lineTo(x - canvas.height, canvas.height);
-            context.stroke();
-        }
+    context.strokeStyle = stripeColor;
+    context.lineWidth = stripeWidth;
+
+    for (let x = 0; x <= canvas.width + canvas.height; x += stripeSpacing) {
+        context.beginPath();
+        context.moveTo(x, 0);
+        context.lineTo(x - canvas.height, canvas.height);
+        context.stroke();
     }
+}
 
-    const textBox = document.getElementById('textBox');
-    const text = textBox.value;
+async function drawImage(context, canvas) {
+    const imageInput = document.getElementById('imageInput');
+    if (!imageInput.files.length) return;
+
+    const image = new Image();
+    image.src = URL.createObjectURL(imageInput.files[0]);
+    await image.decode();
+
+    context.drawImage(
+        image,
+        (canvas.width - image.width) / 2,
+        (canvas.height - image.height) / 2,
+        image.width,
+        image.height
+    );
+
+    URL.revokeObjectURL(image.src);
+}
+
+function drawText(context, canvas) {
+    const text = document.getElementById('textBox');
+    if (!text.value) return;
+
     context.font = '10px visitor';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
-    const measure = context.measureText(text);
-    const typeText = document.getElementById('typeText').firstChild.textContent.toLowerCase();
 
-    const imageInput = document.getElementById('imageInput');
-    if (imageInput.files.length > 0) {
-        const image = await new Promise((resolve, reject) => {
-            const img = new Image();
-            img.onload = () => resolve(img);
-            img.onerror = reject;
-            img.src = URL.createObjectURL(imageInput.files[0]);
-        });
+    const checkbox = document.getElementById('checkboxBorderText');
 
-        context.drawImage(image, (canvas.width - image.width) / 2, (canvas.height - image.height) / 2, image.width, image.height);
-    }
-
-    const checkboxBorderText = document.getElementById('checkboxBorderText');
-    if (checkboxBorderText.checked) {
-        context.strokeStyle = 'black';
+    if (checkbox.checked) {
+        context.strokeStyle = '#000';
         context.lineWidth = 2;
-        context.strokeText(text, canvas.width / 2, canvas.height / 2);
+        context.strokeText(text.value, canvas.width / 2, canvas.height / 2);
     }
 
-    if (typeText === 'solid') context.fillStyle = '#fff';
+    const measure = context.measureText(text.value);
+    const option = document.getElementById('typeText').firstChild.textContent.toLowerCase();
+
+    if (option === 'solid') context.fillStyle = '#fff';
     else {
         const gradient = context.createLinearGradient(
             0,
@@ -75,38 +107,44 @@ export async function createCanvas() {
         gradient.addColorStop(1, '#888');
         context.fillStyle = gradient;
     }
-    context.fillText(text, canvas.width / 2, canvas.height / 2);
+    context.fillText(text.value, canvas.width / 2, canvas.height / 2);
+}
 
-    const checkboxShine = document.getElementById('checkboxShine');
-    if (checkboxShine.checked) {
-        context.beginPath();
-        context.ellipse(
-            canvas.width / 2,
-            0,
-            canvas.width / 2, canvas.height / 2,
-            0,
-            Math.PI,
-            0,
-            true
-        );
-        context.fillStyle = 'rgba(255,255,255,0.2)';
-        context.fill();
-    }
+function drawShine(context, canvas) {
+    const checkbox = document.getElementById('checkboxShine');
+    if (!checkbox.checked) return;
 
-    const checkboxBorder = document.getElementById('checkboxBorder');
-    if (checkboxBorder.checked) {
-        context.strokeStyle = 'black';
-        context.lineWidth = 2;
-        context.strokeRect(0, 0, canvas.width, canvas.height);
-    }
+    context.beginPath();
+    context.ellipse(
+        canvas.width / 2,
+        0,
+        canvas.width / 2, canvas.height / 2,
+        0,
+        Math.PI,
+        0,
+        true
+    );
+    context.fillStyle = 'rgba(255,255,255,0.2)';
+    context.fill();
+}
 
+function drawBorder(context, canvas) {
+    const checkbox = document.getElementById('checkboxBorder');
+    if (!checkbox.checked) return;
+
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+}
+
+function createLargeCanvas(canvas) {
     const canvasLarge = document.createElement('canvas');
     canvasLarge.width = canvas.width * 2;
     canvasLarge.height = canvas.height * 2;
 
-    const contextLarge = canvasLarge.getContext('2d');
-    contextLarge.imageSmoothingEnabled = false;
-    contextLarge.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvasLarge.width, canvasLarge.height);
+    const context = canvasLarge.getContext('2d');
+    context.imageSmoothingEnabled = false;
+    context.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvasLarge.width, canvasLarge.height);
 
-    container.append(canvas, canvasLarge);
+    return canvasLarge;
 }
